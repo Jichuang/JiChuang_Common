2c0bfe92f1fe68837374a2a13e204c6ae539475c
Zhao Peng <hope6537@qq.com>
2015-01-26 14:56:40 +0800

    swift OOP 构造器 5种 析构器 ARC计数GC 强引用 弱引用 闭环 无主引用

--- swift_src/oop.swift
+++ swift_src/oop.swift
@@ -1,5 +1,5 @@
 //
-//  oop=.swift
+//  main.swift
 //  SwiftOOP
 //
 //  Created by Hope6537 on 15-1-13.
@@ -136,4 +136,137 @@ class Assistant_Son:Assistant{
     }
 }
 Assistant_Son.staticMethods();
+//构造类的实例
+class Buddna {
+    var name : String;
+    //这是普通的指定构造器
+    init(theFirstMethod name:String){
+        println("first");
+        self.name = name;
+    }
+    //构造器可以重载
+    init(theSecondMethod name:String){
+        println("second");
+        self.name = name;
+    }
+    //便利构造器 调用其他构造器的方法
+    convenience init (){
+        println("convenience");
+        self.init(theSecondMethod:"theFirst");
+    }
+    //可失败构造器，用于不符合条件时返回null对象
+    init?(name : String){
+        self.name = name;
+        //返回失败必须在最后
+        if(name == "failed"){
+            return nil;
+        }
+    }
+    deinit{
+        println("destroy");
+    }
+}
+//调用构造方法的同时 我们对这个对象进行了创建 并具有一个强引用
+var buddna1:Buddna? = Buddna(theFirstMethod: "first");
+var buddna2 = Buddna(theSecondMethod: "second");
+//释放资源 调用destory方法
+//在1中可以直接赋值为nil 因为在定义的时候我们使用了可选类型
+buddna1 = nil;
+//否则就像2一样 只能由Buddna类型来装载
+buddna2 = Buddna(theFirstMethod: "123");
+//指定构造器中的链式调用父类构造器
+class base1 {
+    var name1 : String;
+    init(){
+        self.name1 = "test";
+        println("base init \(name1)" );
+    }
+}
+class base2 : base1{
+    var name2 : String;
+    override init(){
+        self.name2 = "base2";
+        println("base2 name \(name2)");
+        //显示的调用superinit之后彩才可以使父类的值
+        super.init();
+        self.name1 = "name1";
+    }
+}
+/*
+output:
+base2 name base2
+base init test
+*/
+var testBase = base2();
+//要记住的是，在Swift中除了Class之外，其他的全都是值类型
+//值类型有个方法叫做变质方法。用于修改当前的实例，而不是通过当前实例的基础上来修改属性
 
+struct WarCraft {
+    var sence : String;
+    var money : Int;
+    mutating func jumpToSence(sence:String){
+         //在这里直接生产一个新的实例
+        self = WarCraft(sence: "new Sence", money: 123);
+    }
+    func toString(){
+        println("sence " + self.sence);
+        println("money  \(self.money)");
+    }
+}
+
+func testMutiating(){
+    var myWarCraft = WarCraft(sence: "sence1", money: 1100);
+    myWarCraft.toString();
+    //after
+    myWarCraft.jumpToSence("die");
+    myWarCraft.toString();
+}
+testMutiating();
+//闭环的强引用，即发生内存泄露的元凶
+class Male{
+    let name : String;
+//   使用强引用
+//   var girl : Female?;
+//   使用弱引用
+//    weak var girl : Female?;
+//    使用无主引用 即主人死亡之后 该实例自动销毁 或者说计数－1
+//    即girl死后 boy也殉情了
+    unowned var girl : Female;
+//  在无主引用中时，构造器必须给定无主引用变量的值
+//    init(name : String){
+//        self.name = name;
+//    }
+    init(name : String , girl : Female){
+        self.name = name;
+        self.girl = girl;
+    }
+    deinit{
+        println("die male");
+    }
+}
+
+class Female {
+    let name : String;
+    weak var boy : Male?;
+    init (name : String){
+        self.name = name;
+    }
+    deinit{
+        println("die fe");
+    }
+}
+func deadLock(){
+    //出现了内存泄露
+    var woman : Female? = Female(name:"female");
+    var man : Male? = Male(name: "male",girl:woman!);
+    //在无主引用中要使用！来进行强制解封来使用里面的值
+    //man!.girl =  woman;
+
+    woman!.boy = man;
+    println("deinit");
+    //无主引用中 girl死了boy也会跟着死
+    //man = nil;
+    woman = nil;
+    println("finish");
+}
+deadLock();
